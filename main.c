11cf21c953f3508784064b55f5cfc93becc30edb
i116iked <i520116x@mails.cc.ehime-u.ac.jp>
2023-07-24 20:20:31 +0900

    101個確保

--- main.c
+++ main.c
@@ -15,7 +15,7 @@ double myRoot(double x) // x の平方根を返す関数。x >= 0 ではない�
 int main(void)
 {
   int i;
-  double *x = (double *)malloc(sizeof(double) * 100);
+  double *x = (double *)malloc(sizeof(double) * 101);
 
   while( i <= 100 ){
     x[i] = myRoot((double)i);
